03a80e341003c955dd36cb8f95bbcaa049bd98d1
Swiggui <figueredojuan2020@outlook.es>
2022-11-02 17:08:30 -0500

    hotfix aboutUs

--- aboutUs.php
+++ aboutUs.php
@@ -48,95 +48,95 @@
       </div>
     </nav>
   </header>
-  <div id="login" class="modal fade" tabindex="-1" aria-hidden="true" aria-labelledby="Login">
-    <div class="modal-dialog modal-dialog-centerphp
-      <div class="modal-content">
-        <div class="modal-header d-flex">
-          <h1 class="logo w-100">
-            <a class="navbar-brand fw-bold modal-title" href="./index.php">W31rd W3bs1t3</a>
-            <a class="navbar-brand fw-bold modal-title dotcom" href="./index.php">.com</a>
-          </h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body px-5">
-          <form action="" method="post">
-            <h2 class="modal-title">Ingresar</h2>
-            <div class="form-floating mb-3">
-              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
-              <label for="floatingInput">Usuario</label>
-            </div>
-            <div class="form-floating">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
-              <label for="floatingPassword">Contraseña</label>
-            </div>
-        </div>
-        </form>
-        <div class="modal-footer">
-          <div class="col d-flex">
-            <button type="submit" class="mx-auto w-75 btn btn-outline-primary">¡Ingresa, y
-              admira un nuevo mundo!</button>
-          </div>
-          <div class="col d-flex">
-            <button type="button" class="mx-auto w-75 btn btn-outline-success" data-bs-target="#signin"
-              data-bs-toggle="modal">Que mal que no tengas cuenta... ¡Pero puedes crearte una!</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div id="signin" class="modal fade" tabindex="-1" aria-hidden="true" aria-labelledby="Sign in">
-    <div class="modal-dialog modal-dialog-scrollable">
-      <div class="modal-content">
-        <div class="modal-header d-flex">
-          <h1 class="logo w-100">
-            <a class="navbar-brand fw-bold modal-title" href="./index.php">W31rd W3bs1t3</a>
-            <a class="navbar-brand fw-bold modal-title dotcom" href="./index.php">.com</a>
-          </h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body px-5">
-          <form action="" method="post">
-            <h2 class="modal-title">Registrarse</h2>
-            <div class="form-floating">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
-              <label for="floatingPassword">Nombres</label>
-            </div>
-            <div class="form-floating">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
-              <label for="floatingPassword">Apellidos</label>
-            </div>
-            <div class="form-floating">
-              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
-              <label for="floatingInput">Usuario</label>
-            </div>
-            <div class="form-floating">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
-              <label for="floatingPassword">Contraseña</label>
-            </div>
-            <div class="form-floating">
-              <input type="email" class="form-control" id="floatingInputGroup2" placeholder="Username" required>
-              <label for="floatingInputGroup2">Correo</label>
-            </div>
-            <div class="form-floating">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
-              <label for="floatingPassword">Número de celular</label>
-            </div>
-        </div>
-        </form>
-        <div class="modal-footer">
-          <div class="col d-flex">
-            <button type="button" class="mx-auto w-75 btn btn-outline-primary" data-bs-target="#login"
-              data-bs-toggle="modal">¿Así que ya tenías una cuenta? ¡Ingresa de una vez por todas!</button>
-          </div>
-          <div class="col d-flex">
-            <button type="submit" class="mx-auto w-75 btn btn-outline-success">¡Todo listo para continuar
-              Capitán!</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
   <main class="container pb-sm-1 mb-3">
+  <div id="login" class="modal fade" tabindex="-1" aria-hidden="true" aria-labelledby="Login">
+  <div class="modal-dialog modal-dialog-center">
+    <div class="modal-content">
+      <div class="modal-header d-flex">
+        <h1 class="logo w-100">
+          <a class="navbar-brand fw-bold modal-title" href="./index.php">W31rd W3bs1t3</a>
+          <a class="navbar-brand fw-bold modal-title dotcom" href="./index.php">.com</a>
+        </h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body px-5">
+        <form action="" method="post">
+          <h2 class="modal-title">Ingresar</h2>
+          <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
+            <label for="floatingInput">Usuario</label>
+          </div>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
+            <label for="floatingPassword">Contraseña</label>
+          </div>
+      </div>
+      </form>
+      <div class="modal-footer">
+        <div class="col d-flex">
+          <button type="submit" class="mx-auto w-75 btn btn-outline-primary">¡Ingresa, y
+            admira un nuevo mundo!</button>
+        </div>
+        <div class="col d-flex">
+          <button type="button" class="mx-auto w-75 btn btn-outline-success" data-bs-target="#signin"
+            data-bs-toggle="modal">Que mal que no tengas cuenta... ¡Pero puedes crearte una!</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="signin" class="modal fade" tabindex="-1" aria-hidden="true" aria-labelledby="Sign in">
+  <div class="modal-dialog modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header d-flex">
+        <h1 class="logo w-100">
+          <a class="navbar-brand fw-bold modal-title" href="./index.php">W31rd W3bs1t3</a>
+          <a class="navbar-brand fw-bold modal-title dotcom" href="./index.php">.com</a>
+        </h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body px-5">
+        <form action="" method="post">
+          <h2 class="modal-title">Registrarse</h2>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
+            <label for="floatingPassword">Nombres</label>
+          </div>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
+            <label for="floatingPassword">Apellidos</label>
+          </div>
+          <div class="form-floating">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
+            <label for="floatingInput">Usuario</label>
+          </div>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
+            <label for="floatingPassword">Contraseña</label>
+          </div>
+          <div class="form-floating">
+            <input type="email" class="form-control" id="floatingInputGroup2" placeholder="Username" required>
+            <label for="floatingInputGroup2">Correo</label>
+          </div>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
+            <label for="floatingPassword">Número de celular</label>
+          </div>
+      </div>
+      </form>
+      <div class="modal-footer">
+        <div class="col d-flex">
+          <button type="button" class="mx-auto w-75 btn btn-outline-primary" data-bs-target="#login"
+            data-bs-toggle="modal">¿Así que ya tenías una cuenta? ¡Ingresa de una vez por todas!</button>
+        </div>
+        <div class="col d-flex">
+          <button type="submit" class="mx-auto w-75 btn btn-outline-success">¡Todo listo para continuar
+            Capitán!</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
     <div class="clearfix px-5 pt-5">
       <img src="https://images.hdqwalls.com/wallpapers/fox-snow-qi.jpg" class="col-md-4 float-md-end my-3 ms-md-3" alt="...">
       
